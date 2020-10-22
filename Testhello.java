50a038fd24eee131b975afb436dfa772fd9248df
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-21 23:18:17 -0800

    version2

--- Testhello.java
+++ Testhello.java
@@ -3,10 +3,10 @@ class Testhello
 public static void main (String args[])
 {
 int a=30,b=40,c;
-c=a*b;
+c=a+b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result multiplication two number is :" +c);
+System.out.println ("The result addition two number is :" +c);
 
 }
 

791c58a85b08f28ec0821138a37d014559f1b3af
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-21 22:56:57 -0800

    version1

---
+++ Testhello.java
@@ -0,0 +1,13 @@
+class Testhello
+{
+public static void main (String args[])
+{
+int a=30,b=40,c;
+c=a*b;
+System.out.println ("The first number is :"+a);
+System.out.println ("The second number is :"+b);
+System.out.println ("The result multiplication two number is :" +c);
+
+}
+
+}

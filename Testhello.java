b8bd381f443949bc86d8cc46f3e3c92ebe4d6405
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-21 23:45:09 -0800

    version6

--- Testhello.java
+++ Testhello.java
@@ -6,7 +6,7 @@ int a=70,b=40,c;
 c=a-b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result addition two number is :" +c);
+System.out.println ("The result subtraction two number is :" +c);
 
 }
 

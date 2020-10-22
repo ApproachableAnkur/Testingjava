a5f1a1789c6a2cac1745262fcb007f5fe50cf598
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-10-21 23:21:39 -0800

    version3

--- Testhello.java
+++ Testhello.java
@@ -2,11 +2,11 @@ class Testhello
 {
 public static void main (String args[])
 {
-int a=30,b=40,c;
-c=a+b;
+int a=70,b=40,c;
+c=a-b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
-System.out.println ("The result addition two number is :" +c);
+System.out.println ("The result subtraction two number is :" +c);
 
 }
 
